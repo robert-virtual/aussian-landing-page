28935fec29e8008cd3e0327a4e0c7dc77af27b05
JoseReyes1504 <88816110+JoseReyes1504@users.noreply.github.com>
2024-06-28 00:51:02 -0600

    Animaciones

---
+++ js/animations.js
@@ -0,0 +1,26 @@
+let animado = document.querySelectorAll('.Animado');
+let animado2 = document.querySelectorAll('.Animado2');
+const style = document.documentElement.style;
+var Toques = 0;
+
+function mostrarScroll() {
+    let scrollTop = document.documentElement.scrollTop;
+    for (var i = 0; i < animado.length; i++) {
+        let AlturaAnimado = animado[i].offsetTop;
+        if (AlturaAnimado - 500 < scrollTop) {
+            animado[i].style.opacity = 1;
+            animado[i].classList.add("mostrarArriba");
+        }
+    }
+
+    for (var i = 0; i < animado2.length; i++) {
+        let AlturaAnimado = animado2[i].offsetTop;
+        if (AlturaAnimado - 500 < scrollTop) {
+            animado2[i].style.opacity = 1;
+            animado2[i].classList.add("MostrarLado");
+        }
+    }
+}
+
+window.addEventListener('scroll', mostrarScroll);
+
